0cb6f84dea238040714834f2accdffcc1375ef28
指尖上的艺术 <george@chengguo.io>
2018-09-07 20:05:14 +0800

    Add UML file

--- library/src/main/java/io/chengguo/streaming/RTSPClient.java
+++ library/src/main/java/io/chengguo/streaming/RTSPClient.java
@@ -1,8 +1,12 @@
 package io.chengguo.streaming;
 
+import java.net.Socket;
+
 /**
  * Created by fingerart on 2018-07-15.
  */
 public class RTSPClient {
-
+    public static void main(String[] args) {
+        Socket socket = new Socket();
+    }
 }
